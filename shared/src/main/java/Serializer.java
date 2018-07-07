93aa801faffff92c114cb148089b6114e8f2024d
Dru Hunsaker <druhunsaker@Drus-MacBook.local>
2018-07-06 18:58:14 -0600

    Paul added classes ICommand, and GenericCommand

--- shared/src/main/java/Serializer.java
+++ shared/src/main/java/Serializer.java
@@ -1,5 +1,23 @@
 import com.google.gson.Gson;
+import java.io.Reader;
+
+import sun.net.www.content.text.Generic;
+
 
 public class Serializer {
+
+    public static String serialize(Object obj)
+    {
+        Gson gson = new Gson();
+        String serializedObj = gson.toJson(obj);
+        return serializedObj;
+    }
+
+    public static GenericCommand deserializeCommand(Reader reader)
+    {
+        Gson gson = new Gson();
+        GenericCommand cmd = gson.fromJson(reader, GenericCommand.class);
+        return cmd;
+    }
 
 }
