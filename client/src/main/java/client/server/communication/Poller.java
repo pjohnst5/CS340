cc4fe78546e4ec814b7a6bb0b57fb987473dfc66
Dallas Williamson <i.am.dallas@hotmail.com>
2018-07-07 12:46:27 -0600

    Added Basic Poller Skeletons
    Created server communication package in client module and added the basic components needed for pollers without the capabaility of actually calling the server.

---
+++ client/src/main/java/client/server/communication/Poller.java
@@ -0,0 +1,66 @@
+package client.server.communication;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public abstract class Poller {
+
+    private static boolean _initialized = false;
+    private static Timer _timer;
+    private static Map<String, PollerTask> _callbacks;
+
+    protected Poller(){
+        if (!_initialized) {
+            _callbacks = new HashMap<>();
+            _timer = new Timer();
+            _initialized = true;
+        }
+    }
+
+    protected boolean _start(String identifier, int interval){
+
+        if (!_callbacks.containsKey(identifier)) return false;
+
+        PollerTask task = _callbacks.get(identifier);
+        if (task.isRunning()) return true;
+
+        task.setIsRunning(true);
+
+        _timer.schedule(timerTask(identifier), task.getInterval());
+
+        return true;
+    }
+
+    private TimerTask timerTask(String identifier) {
+        PollerTask task = _callbacks.get(identifier);
+        return new TimerTask() {
+
+            private String _identifier = identifier;
+
+            @Override
+            public void run() {
+                if (!_callbacks.get(_identifier).isRunning()) return;
+
+                _callbacks.get(identifier).getCallback().run();
+                _timer.schedule(timerTask(identifier), _callbacks.get(_identifier).getInterval());
+
+            }
+        };
+    }
+
+    protected boolean _stop(String identifier){
+
+        if (!_callbacks.containsKey(identifier)) return false;
+
+        _callbacks.get(identifier).setIsRunning(false);
+        return true;
+
+    }
+
+    protected void addCallback (String identifier, PollerTask task){
+            _callbacks.put(identifier, task);
+    }
+
+}
