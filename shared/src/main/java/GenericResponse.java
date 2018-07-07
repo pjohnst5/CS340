0250d43306b3038e8f5489ab131b7920b660ad81
Paul Johnston <pjohnst5@icloud.com>
2018-07-06 19:18:26 -0600

    Paul added GenericResponse

---
+++ shared/src/main/java/GenericResponse.java
@@ -0,0 +1,35 @@
+public class GenericResponse {
+    private GenericCommand[] commands;
+    private boolean success;
+    private String errorMessage;
+
+    public void setCommands(GenericCommand[] cmds)
+    {
+        commands = cmds;
+    }
+
+    public void setSucess(boolean s)
+    {
+        success = s;
+    }
+
+    public void setErrorMessage(String s)
+    {
+        errorMessage = s;
+    }
+    
+    public GenericCommand[] getCommands()
+    {
+        return commands;
+    }
+
+    public boolean getSuccess()
+    {
+        return success;
+    }
+
+    public String getErrorMessage()
+    {
+        return errorMessage;
+    }
+}
