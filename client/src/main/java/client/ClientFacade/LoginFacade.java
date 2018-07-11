a06eb53954f4875657a8922f053b4f130406dc9f
Ryan Struthers <ryan.s.struthers@gmail.com>
2018-07-11 12:55:41 -0600

    added login facade

--- client/src/main/java/client/ClientFacade/LoginFacade.java
+++ client/src/main/java/client/ClientFacade/LoginFacade.java
@@ -1,4 +1,21 @@
 package client.ClientFacade;
 
+import shared.Command.GenericCommand;
+
 public class LoginFacade {
+
+    public static void login(String userName, String password) {
+        String[] paramTypes = {String.class.getCanonicalName(), String.class.getCanonicalName()};
+        Object[] paramValues = {userName, password};
+
+        GenericCommand loginCommand = new GenericCommand(
+                "server.ServerFacade.ServerFacade",
+                "login",
+                paramTypes,
+                paramValues,
+                null);
+    }
+
+    public static void register(String username, String password){
+    }
 }
