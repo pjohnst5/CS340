bbed6529ff869cbd3f356e9d1eb8b3755bb0ac0c
jtcooper <joshcooper537@gmail.com>
2018-07-11 13:02:47 -0600

    trying login

---
+++ client/src/main/java/client/server/task/JoinGameTask.java
@@ -0,0 +1,13 @@
+package client.server.task;
+
+public class JoinGameTask {
+    private int _gameId;
+
+    public int get_gameId() {
+        return _gameId;
+    }
+
+    public void set_gameId(int _gameId) {
+        this._gameId = _gameId;
+    }
+}
