3e5e89ea1803f0fbc4a23eea63cc2e7b0724dde7
Ryan Struthers <ryan.s.struthers@gmail.com>
2018-07-12 19:16:27 -0600

    made commandManager facade methods

--- client/src/main/java/client/ClientFacade/CommandFacade.java
+++ client/src/main/java/client/ClientFacade/CommandFacade.java
@@ -1,11 +1,36 @@
 package client.ClientFacade;
 
 import client.ClientModel;
+import shared.Game;
+import shared.Message;
+import shared.Player;
 import shared.User;
 
 public class CommandFacade {
     static ClientModel clientModel = ClientModel.getInstance();
+
     public static void setUser(User user){
         clientModel.setUser(user);
     }
+
+    public static void addGame(Game game) {
+        clientModel.addGameToList(game);
+    }
+
+    public static void addPlayer(Player player){
+        clientModel.addPlayer(player);
+    }
+
+    public static void startGame(String gameId){
+        clientModel.startGame(gameId);
+    }
+
+    public static void sendMessage(Message message){
+        clientModel.sendMessage(message);
+    }
+
+    //create game addGame(Game game)
+    //join game (adding player to specific and setting current game if it's you) addPlayer(Player player)
+    //start game (bool started checking the current game) start(String GameId)
+    //send message sendMessage(Message message)
 }
--- client/src/main/java/client/ClientFacade/GameLobbyFacade.java
+++ client/src/main/java/client/ClientFacade/GameLobbyFacade.java
@@ -4,23 +4,24 @@ package client.ClientFacade;
 import client.server.communication.ServerProxy;
 import shared.Command.GenericCommand;
 import shared.CustomExceptions.ServerProxyException;
+import shared.Message;
 import shared.configuration.ConfigurationManager;
 
 public class GameLobbyFacade {
     static ServerProxy serverProxy = ServerProxy.instance();
 
-    public static void leaveGame(String userName, String gameID) throws ServerProxyException {
-        String[] paramTypes = {String.class.getCanonicalName(), String.class.getCanonicalName()};
-        Object[] paramValues = {userName, gameID};
-
-        GenericCommand command = new GenericCommand(
-                ConfigurationManager.getString("server_facade_name"),
-                ConfigurationManager.getString("leaveGame"),
-                paramTypes,
-                paramValues,
-                null);
-        serverProxy.sendCommand(command);
-    }
+//    public static void leaveGame(String userName, String gameID) throws ServerProxyException {
+//        String[] paramTypes = {String.class.getCanonicalName(), String.class.getCanonicalName()};
+//        Object[] paramValues = {userName, gameID};
+//
+//        GenericCommand command = new GenericCommand(
+//                ConfigurationManager.getString("server_facade_name"),
+//                ConfigurationManager.getString("leaveGame"),
+//                paramTypes,
+//                paramValues,
+//                null);
+//        serverProxy.sendCommand(command);
+//    }
 
     public static void startGame(String gameID) throws ServerProxyException {
         String[] paramTypes = {String.class.getCanonicalName()};
