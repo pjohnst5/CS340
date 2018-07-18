c857a8032a0a72f5937f6561ef0d1c0cf6ea8bad
Paul Johnston <pjohnst5@icloud.com>
2018-07-17 18:56:18 -0600

    Train deck finished

--- shared/src/main/java/shared/model/decks/TrainCardDeck.java
+++ shared/src/main/java/shared/model/decks/TrainCardDeck.java
@@ -12,48 +12,95 @@ public class TrainCardDeck {
     private FaceUpDeck _faceUpDeck;
     private FaceDownDeck _faceDownDeck;
 
+    //-------------------------------------------Public Functions---------------------------------------------//
     public TrainCardDeck()
     {
         final int numNormalCards = 96;
         final int numLocomotiveCards = 10;
         final int numFaceUpCards = 5;
-        List<TrainCard> cards = new ArrayList<>();
+        List<TrainCard> tempDeck = new ArrayList<>();
 
-        //Initialize deck
+        //Initialize temp deck
         for (int i = 0; i < numNormalCards; i++)
         {
             if (i >= 0 && i < 12){
-                cards.add(new TrainCard(TrainColor.PINK));
+                tempDeck.add(new TrainCard(TrainColor.PINK));
             } else if (i >= 12 && i < 24){
-                cards.add(new TrainCard(TrainColor.WHITE));
+                tempDeck.add(new TrainCard(TrainColor.WHITE));
             } else if (i >= 24 && i < 36) {
-                cards.add(new TrainCard(TrainColor.BLUE));
+                tempDeck.add(new TrainCard(TrainColor.BLUE));
             } else if (i >= 36 && i < 48) {
-                cards.add(new TrainCard(TrainColor.YELLOW));
+                tempDeck.add(new TrainCard(TrainColor.YELLOW));
             } else if (i >= 48 && i < 60) {
-                cards.add(new TrainCard(TrainColor.ORANGE));
+                tempDeck.add(new TrainCard(TrainColor.ORANGE));
             } else if (i >= 60 && i < 72) {
-                cards.add(new TrainCard(TrainColor.BLACK));
+                tempDeck.add(new TrainCard(TrainColor.BLACK));
             } else if (i >= 72 && i < 84) {
-                cards.add(new TrainCard(TrainColor.RED));
+                tempDeck.add(new TrainCard(TrainColor.RED));
             } else if (i >= 84 && i < 96) {
-                cards.add(new TrainCard(TrainColor.GREEN));
+                tempDeck.add(new TrainCard(TrainColor.GREEN));
             }
         }
         for (int i = 0; i < numLocomotiveCards; i++)
         {
-            cards.add(new TrainCard(TrainColor.LOCOMOTIVE));
+            tempDeck.add(new TrainCard(TrainColor.LOCOMOTIVE));
         }
 
-        //Shuffle deck
-        Collections.shuffle(cards);
+        //Shuffle temp deck
+        Collections.shuffle(tempDeck);
 
-        //Take first five and put them face up
+        //Instantiate new face up and face down decks
+        _faceDownDeck = new FaceDownDeck();
+        _faceUpDeck = new FaceUpDeck();
+
+        //Sets tempDeck as face down deck
+        for (int i = 0; i < tempDeck.size(); i++){
+            try {
+                _faceDownDeck.addCard(tempDeck.get(i));
+            } catch(DeckException e)
+            {
+                System.out.println("Something cray cray happened when making a new TrainCardDeck: " + e.get_message());
+            }
+        }
+
+        //clears tempDeck
+        tempDeck.clear();
+
+        //Take first five face down cards and put them face up
         for (int i = 0; i < numFaceUpCards; i++){
-
+            try {
+                _faceUpDeck.addCard(_faceDownDeck.drawCard());
+            } catch(DeckException e){
+                System.out.println("Something cray cray happened when making a new TrainCardDeck: " + e.get_message());
+            }
         }
 
     }
+
+    public TrainCard drawFaceDownCard() throws DeckException
+    {
+        return _faceDownDeck.drawCard();
+    }
+
+    public void drawFaceUpCard(TrainCard card) throws DeckException
+    {
+        _faceUpDeck.removeCard(card);
+        _faceUpDeck.addCard(_faceDownDeck.drawCard());
+    }
+
+    public void putTrainCardBack(TrainCard card) throws DeckException {
+        _faceDownDeck.addCard(card);
+    }
+
+    public FaceUpDeck get_faceUpDeck() {
+        return _faceUpDeck;
+    }
+
+    public FaceDownDeck get_faceDownDeck() {
+        return _faceDownDeck;
+    }
+    //---------------------------------------------------------------------------------------------------//
+
 
     private void reDealCards() {
         //adds all face up cards to face down deck
@@ -70,18 +117,13 @@ public class TrainCardDeck {
 
         //Re deals 5 face up cards
         for (int i = 0; i < 5; i++) {
-
+            try {
+                _faceUpDeck.addCard(_faceDownDeck.drawCard());
+            } catch (DeckException e) {
+                System.out.println("Something cray cray happened when redealing the train deck: " + e.get_message());
+            }
         }
     }
-
-    public FaceUpDeck get_faceUpDeck() {
-        return _faceUpDeck;
-    }
-
-    public FaceDownDeck get_faceDownDeck() {
-        return _faceDownDeck;
-    }
-
 
     //FaceUpDeck is private and only TrainDeck has it
     private class FaceUpDeck {
@@ -98,6 +140,11 @@ public class TrainCardDeck {
             if (_cards.size() == 5){
                 throw new DeckException("Already 5 cards in face up deck");
             }
+
+            if (_faceDownDeck._cards.size() + _faceUpDeck._cards.size() == 110){
+                throw new DeckException("There are already 110 cards in the Train deck total : Face Up: " + _faceUpDeck._cards.size() + " face down: " + _cards.size());
+            }
+
             _cards.add(card);
 
             if (card.get_color() == TrainColor.LOCOMOTIVE){
@@ -120,13 +167,12 @@ public class TrainCardDeck {
                 //Card was successfully removed, is it locomotive?
                 if (card.get_color() == TrainColor.LOCOMOTIVE) {
                     _locoMotiveCount--;
-
-                    //TODO: handle locomotive case
                 }
+
             } catch(ClassCastException e){
                 throw new DeckException("Class cast null exception when removing card from face up deck " + e.getMessage());
             } catch(NullPointerException e) {
-                throw new DeckException("remove card face up, requested card is null");
+                throw new DeckException("remove card face up exception, requested card is null");
             }
         }
 
@@ -147,7 +193,10 @@ public class TrainCardDeck {
             return _cards.remove(0);
         }
 
-        private void addCard(TrainCard card) {
+        private void addCard(TrainCard card) throws DeckException {
+            if (_faceDownDeck._cards.size() + _faceUpDeck._cards.size() == 110){
+                throw new DeckException("There are already 110 cards in the Train deck total : Face Up: " + _faceUpDeck._cards.size() + " face down: " + _cards.size());
+            }
             _cards.add(card);
         }
 
