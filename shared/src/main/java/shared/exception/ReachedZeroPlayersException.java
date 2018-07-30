4506041bb88518370c0c633ec63dc0bca17620c5
Ryan Struthers <ryan.s.struthers@gmail.com>
2018-07-30 12:24:46 -0600

    removed unnecessary exceptions

--- shared/src/main/java/shared/exception/ReachedZeroPlayersException.java
+++
@@ -1,8 +0,0 @@
-package shared.exception;
-
-public class ReachedZeroPlayersException extends Exception {
-    public String getMessage()
-    {
-        return "You've reached zero players in the game";
-    }
-}
