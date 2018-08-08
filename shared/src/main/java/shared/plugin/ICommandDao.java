cc4db55554b25b7c1c49f31f510ec4fd78593dc4
Paul Johnston <pjohnst5@icloud.com>
2018-08-08 14:52:46 -0600

    Store command argument is now ICommand

--- shared/src/main/java/shared/plugin/ICommandDao.java
+++ shared/src/main/java/shared/plugin/ICommandDao.java
@@ -2,13 +2,12 @@ package shared.plugin;
 
 import java.util.List;
 
-import shared.command.GenericCommand;
 import shared.command.ICommand;
 import shared.exception.DatabaseException;
 
 public interface ICommandDao {
 
-    void storeCommand(String gameID, int index, GenericCommand command) throws DatabaseException;
+    void storeCommand(String gameID, int index, ICommand command) throws DatabaseException;
 
     List<ICommand> getCommands(String gameID) throws DatabaseException;
 }
