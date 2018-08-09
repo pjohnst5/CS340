536e2539027f46804d6114ed857cabfe7f3cc572
Dallas Williamson <dallas.v.williamson@gmail.com>
2018-08-09 11:50:55 -0600

    Updated For Cross-Compatability

--- shared/src/main/java/shared/configuration/PluginConfiguration.java
+++ shared/src/main/java/shared/configuration/PluginConfiguration.java
@@ -4,28 +4,30 @@ import java.io.File;
 
 public class PluginConfiguration implements IConfiguration {
 
-    private static final String PROTOCOL = "file://";
+    private static final String PROTOCOL = "file:///";
 
 
     @Override
     public void load(IConfigurationManager manager) {
+
+        String workingDir = PROTOCOL +
+                System.getProperty("user.dir") +
+                File.separator + "plugins" +
+                File.separator;
+
+        // Trim extra '/' if you are unix
+        workingDir = workingDir.replaceFirst(PROTOCOL + "/", PROTOCOL);
 
         // Delta Update Interval
         manager.add("delta_update_interval", 10);
 
         // SQL Plugin
         manager.add("plugin_sql_provider_class_name", "sql.provider.Plugin");
-        manager.add("plugin_sql_provider_jar_path", PROTOCOL +
-                System.getProperty("user.dir") +
-                File.separator + "plugins" +
-                File.separator + "sqlprovider.jar");
+        manager.add("plugin_sql_provider_jar_path", workingDir + "sqlprovider.jar");
 
         // JSON Plugin
         manager.add("plugin_json_provider_class_name", "json.provider.Plugin");
-        manager.add("plugin_json_provider_jar_path", PROTOCOL +
-                System.getProperty("user.dir") +
-                File.separator + "plugins" +
-                File.separator + "jsonprovider.jar");
+        manager.add("plugin_json_provider_jar_path", workingDir + "jsonprovider.jar");
 
     }
 }
